ad60915c931f0d91a0126bce78b6d0755e857777
Vilius Valinskis <vilval6@ktu.lt>
2019-03-09 19:00:55 +0200

    Test Discord WebHook

---
+++ WebApp/bin/Debug/netcoreapp2.2/publish/ClientApp/dist/precache-manifest.a4cb07f1af5927c6f5424dbed62a0519.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "33b144c7d29f1770223d",
+    "url": "/js/fetch-data.9675cf0e.js"
+  },
+  {
+    "revision": "3339d905b057f0a47a81",
+    "url": "/js/counter.58d025a1.js"
+  },
+  {
+    "revision": "6fcb75f948792ba038c1",
+    "url": "/js/chunk-vendors.dc53f8cc.js"
+  },
+  {
+    "revision": "26f60e32735e4ed5a08c",
+    "url": "/js/app.3399d7d3.js"
+  },
+  {
+    "revision": "5cd1dfa2e8ea73f3aeb1ad0219f82899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978a7dfc0b332b8f1aeb65e3b869314b",
+    "url": "/img/logo.978a7dfc.png"
+  },
+  {
+    "revision": "6fcb75f948792ba038c1",
+    "url": "/css/chunk-vendors.f6552608.css"
+  },
+  {
+    "revision": "26f60e32735e4ed5a08c",
+    "url": "/css/app.52d55d4e.css"
+  }
+];
