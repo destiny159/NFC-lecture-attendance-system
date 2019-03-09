5670e88dbedbafde0b9f20961477bbadf788c5d5
Vilius Valinskis <vilval6@ktu.lt>
2019-03-09 17:59:58 +0200

    Clean up

---
+++ WebApp/obj/Debug/netcoreapp2.2/PubTmp/Out/ClientApp/dist/precache-manifest.52f9df64366917fe1587798c6f346e1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "25f3925c651258e188c4",
+    "url": "/js/fetch-data.3727067e.js"
+  },
+  {
+    "revision": "9be99f599bb6ce75b7c8",
+    "url": "/js/counter.06817caf.js"
+  },
+  {
+    "revision": "6fcb75f948792ba038c1",
+    "url": "/js/chunk-vendors.dc53f8cc.js"
+  },
+  {
+    "revision": "9c1889d801e35c6da8e9",
+    "url": "/js/app.57f4b203.js"
+  },
+  {
+    "revision": "66f56e7afbce5f91fd86860c05d10fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978a7dfc0b332b8f1aeb65e3b869314b",
+    "url": "/img/logo.978a7dfc.png"
+  },
+  {
+    "revision": "6fcb75f948792ba038c1",
+    "url": "/css/chunk-vendors.f6552608.css"
+  },
+  {
+    "revision": "9c1889d801e35c6da8e9",
+    "url": "/css/app.52d55d4e.css"
+  }
+];
